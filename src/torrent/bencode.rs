78d04bd47b4afbf981a01a77ac0b8702b3bd7896
Adin <andhika.bhayangkara@desaysv.com>
2024-11-14 20:17:22 +0800

    Simplified Torrent data structure

--- src/torrent/bencode.rs
+++ src/torrent/bencode.rs
@@ -3,14 +3,9 @@ use serde_bytes::ByteBuf;
 use serde_derive::Deserialize;
 
 #[derive(Debug, Deserialize)]
-pub struct Node(String, i64);
-
-#[derive(Debug, Deserialize)]
 pub struct File {
     pub path: Vec<String>,
     pub length: i64,
-    #[serde(default)]
-    pub md5sum: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -20,13 +15,9 @@ pub struct Info {
     #[serde(rename = "piece length")]
     pub piece_length: i64,
     #[serde(default)]
-    pub md5sum: Option<String>,
-    #[serde(default)]
     pub length: Option<i64>,
     #[serde(default)]
     pub files: Option<Vec<File>>,
-    #[serde(default)]
-    pub private: Option<u8>,
     #[serde(default)]
     #[serde(rename = "root hash")]
     pub root_hash: Option<String>,
@@ -38,22 +29,11 @@ pub struct Torrent {
     #[serde(default)]
     pub announce: Option<String>,
     #[serde(default)]
-    pub nodes: Option<Vec<Node>>,
-    #[serde(default)]
-    pub encoding: Option<String>,
-    #[serde(default)]
-    pub httpseeds: Option<Vec<String>>,
-    #[serde(default)]
     #[serde(rename = "announce-list")]
     pub announce_list: Option<Vec<Vec<String>>>,
     #[serde(default)]
     #[serde(rename = "creation date")]
     pub creation_date: Option<i64>,
-    #[serde(default)]
-    pub comment: Option<String>,
-    #[serde(default)]
-    #[serde(rename = "created by")]
-    pub created_by: Option<String>,
 }
 
 #[derive(thiserror::Error, Debug)]
