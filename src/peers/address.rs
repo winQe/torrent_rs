adc8c56ae47d23bb56b39aebf182a870a6a13465
Adin <andhika.bhayangkara@desaysv.com>
2024-11-25 18:23:40 +0800

    Added serialization and deserialization for peer addresses

---
+++ src/peers/address.rs
@@ -0,0 +1,60 @@
+// https://github.com/jonhoo/codecrafters-bittorrent-rust/blob/master/src/tracker.rs
+use crate::peers::PeerAddresses;
+
+use serde::de::{self, Deserialize, Deserializer, Visitor};
+use serde::ser::{Serialize, Serializer};
+use std::fmt;
+use std::net::{Ipv4Addr, SocketAddrV4};
+
+struct PeerAddressesVisitor;
+
+impl<'de> Visitor<'de> for PeerAddressesVisitor {
+    type Value = PeerAddresses;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("6 bytes, the first 4 bytes are a peer's IP address and the last 2 are a peer's port number")
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v.len() % 6 != 0 {
+            return Err(E::custom(format!("length is {}", v.len())));
+        }
+        // TODO: use array_chunks when stable; then we can also pattern-match in closure args
+        Ok(PeerAddresses(
+            v.chunks_exact(6)
+                .map(|slice_6| {
+                    SocketAddrV4::new(
+                        Ipv4Addr::new(slice_6[0], slice_6[1], slice_6[2], slice_6[3]),
+                        u16::from_be_bytes([slice_6[4], slice_6[5]]),
+                    )
+                })
+                .collect(),
+        ))
+    }
+}
+
+impl<'de> Deserialize<'de> for PeerAddresses {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(PeerAddressesVisitor)
+    }
+}
+
+impl Serialize for PeerAddresses {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut single_slice = Vec::with_capacity(6 * self.0.len());
+        for peer in &self.0 {
+            single_slice.extend(peer.ip().octets());
+            single_slice.extend(peer.port().to_be_bytes());
+        }
+        serializer.serialize_bytes(&single_slice)
+    }
+}
---
+++ src/peers/mod.rs
@@ -0,0 +1,17 @@
+use std::net::SocketAddrV4;
+
+mod address;
+mod handshake;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PeerAddresses(pub Vec<SocketAddrV4>);
+
+enum PeerState {
+    Choke,
+    Unchoke,
+}
+
+pub struct Peer {
+    addr: SocketAddrV4,
+    state: PeerState,
+}
